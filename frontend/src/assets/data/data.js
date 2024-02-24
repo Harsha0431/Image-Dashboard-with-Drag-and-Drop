34a8e74348b5523f410a86fe3600a0f12b02fe49
Harsha Vardhan Polamarasetty <120405724+Harsha0431@users.noreply.github.com>
2024-02-24 12:57:19 +0530

    Update data.js i.e Spelling mistakes

--- frontend/src/assets/data/data.js
+++ frontend/src/assets/data/data.js
@@ -60,7 +60,7 @@ export const data = [
   },
   {
     id: 7,
-    name: "HELP TO TO IMPROVE",
+    name: "HELP TO US IMPROVE",
     images: [
       "https://images.unsplash.com/photo-1582213782179-e0d53f98f2ca?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       "https://images.unsplash.com/photo-1633613286991-611fe299c4be?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
